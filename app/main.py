b9998cf17b89dd9aa7d1bc72d013f18beb7cacd6
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2026-08-12 16:43:10 -0300

    feat: Refatorar o controlador de movimentação e atualizar o roteamento da aplicação principal

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.controllers import auth_controller
 from app.controllers import usuario_controller
 from app.controllers import categoria_controller
 from app.controllers import produto_controller
-from app.controllers import movimentacao_controller
+from app.controllers import movimentacoes_controller
 
 app = FastAPI(title="Sistema de Ponto de venda")
 
@@ -24,7 +24,7 @@ app.include_router(auth_controller.router)
 app.include_router(usuario_controller.router)
 app.include_router(categoria_controller.router)
 app.include_router(produto_controller.router)
-app.include_router(movimentacao_controller.router)
+app.include_router(movimentacoes_controller.router)
 
 
 @app.get("/")
